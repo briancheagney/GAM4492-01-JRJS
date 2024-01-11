d29c061a051f244c5f1e92b4c05bedef9e734c41
Blazingli0n <ryan.t.keenan@gmail.com>
2024-01-11 14:21:54 -0500

    Revert "Small file changes"
    This reverts commit d9df43c96f0982f120345083c8bb044a09d473ea.

--- GAME/Game/Content/FPS Shooter Recoil Animation 5.2/RecoilAnimation/Source/RecoilAnimation/Private/PluginSettings.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Designed by Hitman's Store, 2022
-
-#include "PluginSettings.h"
-
-UPluginSettings::UPluginSettings(const FObjectInitializer& Obj)
-{
-	CurvesSavePath = DataSavePath = FString("/Game/PRAS/");
-	bCreateDataAssetIfNull = true;
-}
